1cc9f11623bc8c8d72bddc61fdf0fb626eb5e6bb
Mateusz Stroba <mateusz@python.com>
2024-01-24 11:09:47 +0100

    base64_encoding

--- base64_encoding.py
+++ base64_encoding.py
@@ -1,12 +1,20 @@
 import base64
 
 
-def encrypt_pass(password):
+def encode_pass(password):
     encoded_bytes = base64.b64encode(password.encode())
     #print(encoded_bytes)
     return encoded_bytes
 
+def decode_pass(encoded_password):
+    decoded_password = base64.b64decode(encoded_password.decode())
+    #print(encoded_bytes)
+    return decoded_password
 
 user_password = input("Enter your password: ")
 
-print(encrypt_pass(user_password))
+
+encoded_pass = encode_pass(user_password)
+
+print(encoded_pass)
+print(decode_pass(encoded_pass))
