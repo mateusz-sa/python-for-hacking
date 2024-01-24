c00ae95903f70212d1ca6e19180cb037d51883bc
Mateusz Stroba <mateusz@python.com>
2024-01-24 14:01:22 +0100

    index.html

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+student_grades = {}
+
+off = False
+while not off:
+    name = input("Enter student name: ")
+    grade = input("Enter student grade: ")
+    student_grades[name] = grade
+    print("Student added successfully")
+    print(student_grades)
+
+    add_another = input("Would you like add another student? Y or N ").lower()
+    if add_another == "y":
+        pass
+    else: 
+        off = True
+        print(student_grades)
---
+++ main.py
@@ -0,0 +1,12 @@
+dictionary = { "name": "mateusz",
+                "age": "27"}
+
+print(dictionary)
+
+dictionary["id"] = 1
+print(dictionary)
+
+
+for element in dictionary:
+    print(element)
+    print(dictionary[element])
