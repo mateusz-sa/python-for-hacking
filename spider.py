c6c766e96436343c0868c963e9a8d0947e138c35
Mateusz Stroba <mateusz@python.com>
2024-01-25 09:30:49 +0100

    mod

--- spider.py
+++ spider.py
@@ -39,6 +39,6 @@ def spider_urls(url, keyword):
 # https://www.yahoo.com
 
 
-url = input("Eneter the URL you want to scrap. ")
+url = input("Enter the URL you want to scrap. ")
 keyword = input("Enter the keyword to search for in the URL provided. ")
 spider_urls(url, keyword)
